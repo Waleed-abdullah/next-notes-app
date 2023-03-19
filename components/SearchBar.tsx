5b81e213ae7f055e6fcadaa7a4edb55e07c3f34f
Waleed-Abdullah <w26waleed@gmail.com>
2023-03-19 21:30:17 +0500

    added search and filter functionalit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,41 @@
+const SearchBar = (props: any) => {
+  const { setSearch } = props;
+  const changeSearchState = (event: any) => {
+    setSearch(event.target.value);
+  };
+  return (
+    <form>
+      <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          onChange={changeSearchState}
+          className="block w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50 focus:ring-black focus:border-black dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-black dark:focus:border-black"
+          placeholder="Type to filter"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
--- pages/todos/[id].tsx
+++ pages/todos/[id].tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import axios from 'axios';
 import { todoValidator } from '@/validate/validators';
+import NavBar from '@/components/NavBar';
 
 // Define Prop Interface
 interface ShowProps {
@@ -70,56 +71,59 @@ function Show(props: ShowProps) {
 
   //return JSX
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <div className="h-100 h-100 w-full flex items-center justify-center font-sans">
-        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg text-center">
-          <div className="mb-4">
-            <h1 className="text-black font-semibold">
-              Choose <span className="text-xl text-stone-500">Todo</span>{' '}
-              Options
-            </h1>
-          </div>
-          <div className="flex flex-col items-center justify-center space-x-3">
-            <h1>{todo.item}</h1>
-            <div className="flex flex-row items-center justify-center space-x-3">
-              <h2 className="flex-no-shrink p-2 border-b-2 text-black border-black ">
-                {todo.completed ? 'Completed' : 'Incomplete'}
-              </h2>
-              <button
-                className="flex-no-shrink p-2 border-2 rounded text-black border-black
+    <>
+      <NavBar />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+        <div className="h-100 h-100 w-full flex items-center justify-center font-sans">
+          <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg text-center">
+            <div className="mb-4">
+              <h1 className="text-black font-semibold">
+                Choose <span className="text-xl text-stone-500">Todo</span>{' '}
+                Options
+              </h1>
+            </div>
+            <div className="flex flex-col items-center justify-center space-x-3">
+              <h1>{todo.item}</h1>
+              <div className="flex flex-row items-center justify-center space-x-3">
+                <h2 className="flex-no-shrink p-2 border-b-2 text-black border-black ">
+                  {todo.completed ? 'Completed' : 'Incomplete'}
+                </h2>
+                <button
+                  className="flex-no-shrink p-2 border-2 rounded text-black border-black
                hover:text-white hover:bg-gray-700 mt-5 "
-                onClick={handleComplete}
-              >
-                Complete
-              </button>
-              <button
-                className="flex-no-shrink p-2 border-2 rounded text-black border-black
+                  onClick={handleComplete}
+                >
+                  Complete
+                </button>
+                <button
+                  className="flex-no-shrink p-2 border-2 rounded text-black border-black
                hover:text-white hover:bg-gray-700 mt-5 "
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-              <button
-                className="flex-no-shrink p-2 border-2 rounded text-black border-black
+                  onClick={handleDelete}
+                >
+                  Delete
+                </button>
+                <button
+                  className="flex-no-shrink p-2 border-2 rounded text-black border-black
                hover:text-white hover:bg-gray-700 mt-5 "
-                onClick={() => {
-                  router.push('/');
-                }}
-              >
-                Go Back
-              </button>
-              <button
-                className="flex-no-shrink p-2 border-2 rounded text-black border-black
+                  onClick={() => {
+                    router.push('/');
+                  }}
+                >
+                  Go Back
+                </button>
+                <button
+                  className="flex-no-shrink p-2 border-2 rounded text-black border-black
                hover:text-white hover:bg-gray-700 mt-5 "
-                onClick={setShared}
-              >
-                {!todo.shared ? 'Share' : 'Unshare'}
-              </button>
+                  onClick={setShared}
+                >
+                  {!todo.shared ? 'Share' : 'Unshare'}
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
